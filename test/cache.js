faee4bb45274e8d5c888a99db61dba3d70d7d553
Myztiq <kahn@runnable.com>
2016-04-26 17:01:35 -0700

    Added more coverage to cache expiration.

--- test/cache.js
+++ test/cache.js
@@ -6,11 +6,12 @@ var cache = require('../cache')
 describe('cache', function () {
   describe('handleCache', function () {
     var cacheTestMethod
+    var failCase = new Error('Kaaahhhhnnnn')
     beforeEach(function () {
       cacheTestMethod = sinon.stub()
       cacheTestMethod
         .onFirstCall().yields(null, 'first')
-        .onSecondCall().yields(null, 'second')
+        .onSecondCall().yields(failCase)
       process.env.SWARMERODE_CACHE_LENGTH = 10000
     })
 
@@ -27,7 +28,7 @@ describe('cache', function () {
         if (count === 2) {
           sinon.assert.calledTwice(cbTracker)
           sinon.assert.calledWith(cbTracker, null, 'first')
-          sinon.assert.calledWith(cbTracker, null, 'first')
+          sinon.assert.neverCalledWith(cbTracker, failCase)
           sinon.assert.calledOnce(cacheTestMethod)
           done()
         }
@@ -46,7 +47,7 @@ describe('cache', function () {
         if (count === 2) {
           sinon.assert.calledTwice(cbTracker)
           sinon.assert.calledWith(cbTracker, null, 'first')
-          sinon.assert.calledWith(cbTracker, null, 'second')
+          sinon.assert.calledWith(cbTracker, failCase)
           sinon.assert.calledTwice(cacheTestMethod)
           done()
         }
@@ -54,6 +55,28 @@ describe('cache', function () {
       cache.handleCache('cacheTest', cacheTestMethod, cb)
       cache.handleCache('cacheTest', cacheTestMethod, cb)
     })
+
+    it('should expire the cache', function () {
+      process.env.SWARMERODE_CACHE_LENGTH = 10
+      var count = 0
+      var cbTracker = sinon.stub()
+      function cb (err, data) {
+        cbTracker(err, data)
+        count++
+        if (count === 3) {
+          sinon.assert.calledThrice(cbTracker)
+          sinon.assert.calledWith(cbTracker, null, 'first')
+          sinon.assert.calledWith(cbTracker, failCase)
+          sinon.assert.calledOnce(cacheTestMethod)
+          done()
+        }
+      }
+      cache.handleCache('cacheTest', cacheTestMethod, cb)
+      cache.handleCache('cacheTest', cacheTestMethod, cb)
+      setTimeout(function () {
+        cache.handleCache('cacheTest', cacheTestMethod, cb)
+      }, 100)
+    });
   })
 })
 
